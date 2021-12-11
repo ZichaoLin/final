20bd46b95d95fb2d3e00dc51cba46a9dfb2d33e0
your name <your_email@youremail.com>
2021-12-11 11:22:43 -0800

    1

--- src/main/java/sample/main.java
+++ src/main/java/sample/main.java
@@ -2,6 +2,7 @@ package sample;
 
 public class main {
     public static void main(String[] args) {
-
+        savingsController  sc = new savingsController();
+        sc.showSavingstablepage();
     }
 }
